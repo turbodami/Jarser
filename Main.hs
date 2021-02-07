bed9b05a111a7adb47c42130bf84e62f51239c19
Damiano Presa <presadamiano@gmail.com>
2021-02-07 20:39:31 +0100

    main.hs

---
+++ Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ViewPatterns      #-}
+
+module Main where
+
+import           Control.Applicative
+import           Data.Char
+import           Numeric
+import           System.Exit
+
+data Input = Input
+  { inputLoc :: Int
+  , inputStr :: String
+  } deriving (Show, Eq)
+
+-- | Pull the first character of the input if there is one still input
+inputUncons :: Input                  -- input to check
+            -> Maybe (Char, Input)
+inputUncons (Input _ [])       = Nothing
+inputUncons (Input loc (x:xs)) = Just (x, Input (loc + 1) xs)
+
+data JsonValue
+  = JsonNull
+  | JsonBool Bool
+  | JsonNumber Double
+  | JsonString String
+  | JsonArray [JsonValue]
+  | JsonObject [(String, JsonValue)]
+  deriving (Show, Eq)
