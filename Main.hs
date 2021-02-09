83b041c6d67b747c53c6d71dfbee1cfe4a6e9377
Damiano Presa <presadamiano@gmail.com>
2021-02-09 22:06:08 +0100

    main.hs

--- Main.hs
+++ Main.hs
@@ -13,9 +13,66 @@ data Input = Input
   , inputStr :: String
   } deriving (Show, Eq)
 
--- | Pull the first character of the input if there is one still input
-inputUncons :: Input                  -- input to check
+inputUncons :: Input                
             -> Maybe (Char, Input)
 inputUncons (Input _ [])       = Nothing
 inputUncons (Input loc (x:xs)) = Just (x, Input (loc + 1) xs)
 
+data JsonValue
+  = JsonNull
+  | JsonBool Bool
+  | JsonNumber Double
+  | JsonString String
+  | JsonArray [JsonValue]
+  | JsonObject [(String, JsonValue)]
+  deriving (Show, Eq)
+
+data ParserError = ParserError Int String deriving (Show)
+
+newtype Parser a = Parser
+  { runParser :: Input -> Either ParserError (Input, a)
+  }
+
+instance Functor Parser where
+  fmap f (Parser p) =
+    Parser $ \input -> do
+      (input', x) <- p input
+      return (input', f x)
+
+instance Applicative Parser where
+  pure x = Parser $ \input -> Right (input, x)
+  (Parser p1) <*> (Parser p2) =
+    Parser $ \input -> do
+      (input', f) <- p1 input
+      (input'', a) <- p2 input'
+      return (input'', f a)
+
+instance Alternative (Either ParserError) where
+  empty = Left $ ParserError 0 "empty"
+  Left _ <|> e2 = e2
+  e1 <|> _ = e1
+
+instance Alternative Parser where
+  empty = Parser $ const empty
+  (Parser p1) <|> (Parser p2) =
+    Parser $ \input -> p1 input <|> p2 input
+
+jsonNull :: Parser JsonValue
+jsonNull = JsonNull <$ stringP "null"
+
+charP :: Char      
+      -> Parser Char
+charP x = Parser f
+  where
+    f input@(inputUncons -> Just (y, ys))
+      | y == x = Right (ys, x)
+      | otherwise =
+        Left $
+        ParserError
+          (inputLoc input)
+          ("Expected '" ++ [x] ++ "', but found '" ++ [y] ++ "'")
+    f input =
+      Left $
+      ParserError
+        (inputLoc input)
+        ("Expected '" ++ [x] ++ "', but reached end of string")
